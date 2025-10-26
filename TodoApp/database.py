54eb2d372af1f63324d7f12717070cce5e98d85a
Anurag Tekam <anuragtekam0@gmail.com>
2025-10-27 02:43:11 +0530

    Added some extra comments and code explaination

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -5,3 +5,6 @@ SQLALCHEMY_DATABASE_URL =  "sqlite:///./todos.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+#This is demo database application
